9b023e3297f08b7730e8090079a8117ec381080c
Bogusz Laszczyk <laszczykb@student.agh.edu.pl>
2023-10-21 19:01:30 +0200

    added Ancymon character

--- gupb/model/characters.py
+++ gupb/model/characters.py
@@ -14,7 +14,7 @@ from gupb.model import consumables
 from gupb.model import tiles
 from gupb.model import weapons
 
-verbose_logger = logging.getLogger('verbose')
+verbose_logger = logging.getLogger("verbose")
 
 CHAMPION_STARTING_HP: int = 8
 PENALISED_IDLE_TIME = 16
@@ -35,23 +35,26 @@ class ChampionDescription(NamedTuple):
 
 
 class Tabard(Enum):
-    BLUE = 'Blue'
-    BROWN = 'Brown'
-    GREY = 'Grey'
-    GREEN = 'Green'
-    LIME = 'Lime'
-    ORANGE = 'Orange'
-    PINK = 'Pink'
-    RED = 'Red'
-    STRIPPED = 'Stripped'
-    TURQUOISE = 'Turquoise'
-    VIOLET = 'Violet'
-    WHITE = 'White'
-    YELLOW = 'Yellow'
+    BLUE = "Blue"
+    BROWN = "Brown"
+    GREY = "Grey"
+    GREEN = "Green"
+    LIME = "Lime"
+    ORANGE = "Orange"
+    PINK = "Pink"
+    RED = "Red"
+    STRIPPED = "Stripped"
+    TURQUOISE = "Turquoise"
+    VIOLET = "Violet"
+    WHITE = "White"
+    YELLOW = "Yellow"
+    ANCYMON = "Ancymon"
 
 
 class Champion:
-    def __init__(self, starting_position: coordinates.Coords, arena: arenas.Arena) -> None:
+    def __init__(
+        self, starting_position: coordinates.Coords, arena: arenas.Arena
+    ) -> None:
         self.facing: Facing = Facing.random()
         self.weapon: weapons.Weapon = weapons.Knife()
         self.health: int = CHAMPION_STARTING_HP
@@ -68,7 +71,9 @@ class Champion:
         self.tabard = self.controller.preferred_tabard
 
     def description(self) -> ChampionDescription:
-        return ChampionDescription(self.controller.name, self.health, self.weapon.description(), self.facing)
+        return ChampionDescription(
+            self.controller.name, self.health, self.weapon.description(), self.facing
+        )
 
     def verbose_name(self) -> str:
         return self.controller.name if self.controller else "NULL_CONTROLLER"
@@ -77,8 +82,12 @@ class Champion:
         if self.alive:
             self.store_previous_state()
             action = self.pick_action()
-            verbose_logger.debug(f"Champion {self.verbose_name()} picked action {action}.")
-            ChampionPickedActionReport(self.verbose_name(), action.name).log(logging.DEBUG)
+            verbose_logger.debug(
+                f"Champion {self.verbose_name()} picked action {action}."
+            )
+            ChampionPickedActionReport(self.verbose_name(), action.name).log(
+                logging.DEBUG
+            )
             action(self)
             self.arena.stay(self)
             self.assess_idle_penalty()
@@ -88,12 +97,17 @@ class Champion:
         self.previous_facing = self.facing
 
     def assess_idle_penalty(self) -> None:
-        if self.position == self.previous_position and self.previous_facing == self.facing:
+        if (
+            self.position == self.previous_position
+            and self.previous_facing == self.facing
+        ):
             self.time_idle += 1
         else:
             self.time_idle = 0
         if self.time_idle >= PENALISED_IDLE_TIME:
-            verbose_logger.debug(f"Champion {self.verbose_name()} penalised for idle time.")
+            verbose_logger.debug(
+                f"Champion {self.verbose_name()} penalised for idle time."
+            )
             IdlePenaltyReport(self.verbose_name()).log(logging.DEBUG)
             self.damage(IDLE_DAMAGE_PENALTY)
 
@@ -101,31 +115,49 @@ class Champion:
     def pick_action(self) -> Action:
         if self.controller:
             visible_tiles = self.arena.visible_tiles(self)
-            knowledge = ChampionKnowledge(self.position, self.arena.no_of_champions_alive, visible_tiles)
+            knowledge = ChampionKnowledge(
+                self.position, self.arena.no_of_champions_alive, visible_tiles
+            )
             try:
                 action = self.controller.decide(knowledge)
                 if action is None:
-                    verbose_logger.warning(f"Controller {self.verbose_name()} returned a non-action.")
-                    controller.ControllerExceptionReport(self.verbose_name(), "a non-action returned").log(logging.WARN)
+                    verbose_logger.warning(
+                        f"Controller {self.verbose_name()} returned a non-action."
+                    )
+                    controller.ControllerExceptionReport(
+                        self.verbose_name(), "a non-action returned"
+                    ).log(logging.WARN)
                     return Action.DO_NOTHING
                 return action
             except Exception as e:
-                verbose_logger.warning(f"Controller {self.verbose_name()} throw an unexpected exception: {repr(e)}.")
-                controller.ControllerExceptionReport(self.verbose_name(), repr(e)).log(logging.WARN)
+                verbose_logger.warning(
+                    f"Controller {self.verbose_name()} throw an unexpected exception: {repr(e)}."
+                )
+                controller.ControllerExceptionReport(self.verbose_name(), repr(e)).log(
+                    logging.WARN
+                )
                 return Action.DO_NOTHING
         else:
-            verbose_logger.warning(f"Controller {self.verbose_name()} was non-existent.")
-            controller.ControllerExceptionReport(self.verbose_name(), "controller non-existent").log(logging.WARN)
+            verbose_logger.warning(
+                f"Controller {self.verbose_name()} was non-existent."
+            )
+            controller.ControllerExceptionReport(
+                self.verbose_name(), "controller non-existent"
+            ).log(logging.WARN)
             return Action.DO_NOTHING
 
     def turn_left(self) -> None:
         self.facing = self.facing.turn_left()
-        verbose_logger.debug(f"Champion {self.controller.name} is now facing {self.facing}.")
+        verbose_logger.debug(
+            f"Champion {self.controller.name} is now facing {self.facing}."
+        )
         ChampionFacingReport(self.controller.name, self.facing.value).log(logging.DEBUG)
 
     def turn_right(self) -> None:
         self.facing = self.facing.turn_right()
-        verbose_logger.debug(f"Champion {self.controller.name} is now facing {self.facing}.")
+        verbose_logger.debug(
+            f"Champion {self.controller.name} is now facing {self.facing}."
+        )
         ChampionFacingReport(self.controller.name, self.facing.value).log(logging.DEBUG)
 
     def step_forward(self) -> None:
@@ -133,8 +165,12 @@ class Champion:
 
     def attack(self) -> None:
         self.weapon.cut(self.arena, self.position, self.facing)
-        verbose_logger.debug(f"Champion {self.controller.name} attacked with its {self.weapon.description().name}.")
-        ChampionAttackReport(self.controller.name, self.weapon.description().name).log(logging.DEBUG)
+        verbose_logger.debug(
+            f"Champion {self.controller.name} attacked with its {self.weapon.description().name}."
+        )
+        ChampionAttackReport(self.controller.name, self.weapon.description().name).log(
+            logging.DEBUG
+        )
 
     def do_nothing(self) -> None:
         pass
@@ -142,15 +178,21 @@ class Champion:
     def damage(self, wounds: int) -> None:
         self.health -= wounds
         self.health = self.health if self.health > 0 else 0
-        verbose_logger.debug(f"Champion {self.controller.name} took {wounds} wounds, it has now {self.health} hp left.")
-        ChampionWoundsReport(self.controller.name, wounds, self.health).log(logging.DEBUG)
+        verbose_logger.debug(
+            f"Champion {self.controller.name} took {wounds} wounds, it has now {self.health} hp left."
+        )
+        ChampionWoundsReport(self.controller.name, wounds, self.health).log(
+            logging.DEBUG
+        )
         if not self.alive:
             self.die()
 
     def die(self) -> None:
         self.arena.terrain[self.position].character = None
         self.arena.terrain[self.position].consumable = consumables.Potion()
-        self.arena.terrain[self.position].loot = self.weapon if self.weapon.droppable() else None
+        self.arena.terrain[self.position].loot = (
+            self.weapon if self.weapon.droppable() else None
+        )
         verbose_logger.debug(f"Champion {self.controller.name} died.")
         ChampionDeathReport(self.controller.name).log(logging.DEBUG)
 
--- gupb/view/render.py
+++ gupb/view/render.py
@@ -16,7 +16,7 @@ from gupb.model import weapons
 
 pygame.init()
 
-Sprite = TypeVar('Sprite')
+Sprite = TypeVar("Sprite")
 
 INIT_TILE_SIZE = 32
 KEEP_TILE_RATIO = False
@@ -27,12 +27,12 @@ HEALTH_BAR_FULL_COLOR = (250, 0, 0)
 HEALTH_BAR_OVERFILL_COLOR = (150, 0, 0)
 HEALTH_BAR_EMPTY_COLOR = (0, 0, 0)
 
-BLACK = pygame.Color('black')
+BLACK = pygame.Color("black")
 GAME_FONT = pygame.freetype.Font("resources/fonts/whitrabt.ttf", 24)
 
 
 def load_sprite(group: str, name: str, transparent: pygame.Color = None) -> Sprite:
-    path = os.path.join('resources', 'images', group, f'{name}.png')
+    path = os.path.join("resources", "images", group, f"{name}.png")
     sprite = pygame.image.load(path).convert()
     if sprite.get_size() is not (INIT_TILE_SIZE, INIT_TILE_SIZE):
         sprite = pygame.transform.scale(sprite, (INIT_TILE_SIZE, INIT_TILE_SIZE))
@@ -45,35 +45,44 @@ class SpriteRepository:
     def __init__(self) -> None:
         self.size = (INIT_TILE_SIZE, INIT_TILE_SIZE)
         self.sprites: dict[Any, Sprite] = {
-            tiles.Land: load_sprite('tiles', 'land'),
-            tiles.Sea: load_sprite('tiles', 'sea'),
-            tiles.Wall: load_sprite('tiles', 'wall'),
-            tiles.Menhir: load_sprite('tiles', 'menhir'),
-
-            weapons.Knife: load_sprite('weapons', 'knife', BLACK),
-            weapons.Sword: load_sprite('weapons', 'sword', BLACK),
-            weapons.Axe: load_sprite('weapons', 'axe', BLACK),
-            weapons.Bow: load_sprite('weapons', 'bow', BLACK),
-            weapons.Amulet: load_sprite('weapons', 'amulet', BLACK),
-
-            consumables.Potion: load_sprite('consumables', 'potion', BLACK),
-
-            characters.Tabard.BLUE: load_sprite('characters', 'champion_blue', BLACK),
-            characters.Tabard.BROWN: load_sprite('characters', 'champion_brown', BLACK),
-            characters.Tabard.GREY: load_sprite('characters', 'champion_grey', BLACK),
-            characters.Tabard.GREEN: load_sprite('characters', 'champion_green', BLACK),
-            characters.Tabard.LIME: load_sprite('characters', 'champion_lime', BLACK),
-            characters.Tabard.ORANGE: load_sprite('characters', 'champion_orange', BLACK),
-            characters.Tabard.PINK: load_sprite('characters', 'champion_pink', BLACK),
-            characters.Tabard.RED: load_sprite('characters', 'champion_red', BLACK),
-            characters.Tabard.STRIPPED: load_sprite('characters', 'champion_stripped', BLACK),
-            characters.Tabard.TURQUOISE: load_sprite('characters', 'champion_turquoise', BLACK),
-            characters.Tabard.VIOLET: load_sprite('characters', 'champion_violet', BLACK),
-            characters.Tabard.WHITE: load_sprite('characters', 'champion_white', BLACK),
-            characters.Tabard.YELLOW: load_sprite('characters', 'champion_yellow', BLACK),
-
-            effects.Mist: load_sprite('effects', 'mist', BLACK),
-            effects.WeaponCut: load_sprite('effects', 'blood', BLACK),
+            tiles.Land: load_sprite("tiles", "land"),
+            tiles.Sea: load_sprite("tiles", "sea"),
+            tiles.Wall: load_sprite("tiles", "wall"),
+            tiles.Menhir: load_sprite("tiles", "menhir"),
+            weapons.Knife: load_sprite("weapons", "knife", BLACK),
+            weapons.Sword: load_sprite("weapons", "sword", BLACK),
+            weapons.Axe: load_sprite("weapons", "axe", BLACK),
+            weapons.Bow: load_sprite("weapons", "bow", BLACK),
+            weapons.Amulet: load_sprite("weapons", "amulet", BLACK),
+            consumables.Potion: load_sprite("consumables", "potion", BLACK),
+            characters.Tabard.BLUE: load_sprite("characters", "champion_blue", BLACK),
+            characters.Tabard.BROWN: load_sprite("characters", "champion_brown", BLACK),
+            characters.Tabard.GREY: load_sprite("characters", "champion_grey", BLACK),
+            characters.Tabard.GREEN: load_sprite("characters", "champion_green", BLACK),
+            characters.Tabard.LIME: load_sprite("characters", "champion_lime", BLACK),
+            characters.Tabard.ORANGE: load_sprite(
+                "characters", "champion_orange", BLACK
+            ),
+            characters.Tabard.PINK: load_sprite("characters", "champion_pink", BLACK),
+            characters.Tabard.RED: load_sprite("characters", "champion_red", BLACK),
+            characters.Tabard.STRIPPED: load_sprite(
+                "characters", "champion_stripped", BLACK
+            ),
+            characters.Tabard.TURQUOISE: load_sprite(
+                "characters", "champion_turquoise", BLACK
+            ),
+            characters.Tabard.VIOLET: load_sprite(
+                "characters", "champion_violet", BLACK
+            ),
+            characters.Tabard.WHITE: load_sprite("characters", "champion_white", BLACK),
+            characters.Tabard.YELLOW: load_sprite(
+                "characters", "champion_yellow", BLACK
+            ),
+            characters.Tabard.ANCYMON: load_sprite(
+                "characters", "champion_ancymon", BLACK
+            ),
+            effects.Mist: load_sprite("effects", "mist", BLACK),
+            effects.WeaponCut: load_sprite("effects", "blood", BLACK),
         }
         self.rotation_values: dict[characters.Facing, int] = {
             characters.Facing.RIGHT: 0,
@@ -81,8 +90,12 @@ class SpriteRepository:
             characters.Facing.LEFT: 180,
             characters.Facing.DOWN: 270,
         }
-        self.champion_sprites: dict[tuple[characters.Tabard, characters.Facing], Sprite] = {
-            (tabard, facing): pygame.transform.rotate(self.sprites[tabard], self.rotation_values[facing])
+        self.champion_sprites: dict[
+            tuple[characters.Tabard, characters.Facing], Sprite
+        ] = {
+            (tabard, facing): pygame.transform.rotate(
+                self.sprites[tabard], self.rotation_values[facing]
+            )
             for tabard, facing in itertools.product(
                 [
                     characters.Tabard.BLUE,
@@ -98,13 +111,14 @@ class SpriteRepository:
                     characters.Tabard.VIOLET,
                     characters.Tabard.WHITE,
                     characters.Tabard.YELLOW,
+                    characters.Tabard.ANCYMON,
                 ],
                 [
                     characters.Facing.RIGHT,
                     characters.Facing.UP,
                     characters.Facing.LEFT,
                     characters.Facing.DOWN,
-                ]
+                ],
             )
         }
 
@@ -121,8 +135,13 @@ class SpriteRepository:
     def scale_sprite(sprite: Sprite, size: Tuple[int, int]) -> Sprite:
         return pygame.transform.scale(sprite, size)
 
-    def scale_sprites(self, window_size: Tuple[int, int], arena_size: Tuple[int, int]) -> Tuple[int, int]:
-        self.size = (int(window_size[0] / arena_size[0]), int(window_size[1] / arena_size[1]))
+    def scale_sprites(
+        self, window_size: Tuple[int, int], arena_size: Tuple[int, int]
+    ) -> Tuple[int, int]:
+        self.size = (
+            int(window_size[0] / arena_size[0]),
+            int(window_size[1] / arena_size[1]),
+        )
 
         if KEEP_TILE_RATIO:
             self.size = (min(self.size), min(self.size))
@@ -131,14 +150,16 @@ class SpriteRepository:
             self.sprites[sprite] = self.scale_sprite(self._sprites[sprite], self.size)
 
         for sprite in self.champion_sprites:
-            self.champion_sprites[sprite] = self.scale_sprite(self._champion_sprites[sprite], self.size)
+            self.champion_sprites[sprite] = self.scale_sprite(
+                self._champion_sprites[sprite], self.size
+            )
 
         return self.size[0] * arena_size[0], self.size[1] * arena_size[1]
 
 
 class Renderer:
     def __init__(self, ms_per_time_unit: int = 5):
-        pygame.display.set_caption('GUPB')
+        pygame.display.set_caption("GUPB")
         self.screen = pygame.display.set_mode((500, 500), pygame.RESIZABLE)
         self.sprite_repository = SpriteRepository()
         self.clock = pygame.time.Clock()
@@ -146,10 +167,10 @@ class Renderer:
         self.ms_per_time_unit = ms_per_time_unit
 
     def run(
-            self,
-            game: games.Game,
-            show_sight: Optional[characters.Champion] = None,
-            keyboard_controller: Optional[keyboard.KeyboardController] = None,
+        self,
+        game: games.Game,
+        show_sight: Optional[characters.Champion] = None,
+        keyboard_controller: Optional[keyboard.KeyboardController] = None,
     ) -> None:
         self.screen = self._resize_window(game)
         self._render_starting_screen()
@@ -170,12 +191,17 @@ class Renderer:
                 elif event.type == pygame.KEYDOWN and keyboard_controller:
                     keyboard_controller.register(event.key)
                 if event.type == pygame.VIDEORESIZE:
-                    new_size = self.sprite_repository.scale_sprites((event.w, event.h), game.arena.size)
+                    new_size = self.sprite_repository.scale_sprites(
+                        (event.w, event.h), game.arena.size
+                    )
                     self.screen = pygame.display.set_mode(new_size, pygame.RESIZABLE)
 
     def _resize_window(self, game: games.Game) -> pygame.Surface:
         arena_x_size, arena_y_size = game.arena.size
-        window_size = self.sprite_repository.size[0] * arena_x_size, self.sprite_repository.size[1] * arena_y_size
+        window_size = (
+            self.sprite_repository.size[0] * arena_x_size,
+            self.sprite_repository.size[1] * arena_y_size,
+        )
         return pygame.display.set_mode(window_size, pygame.RESIZABLE)
 
     def _time_to_cycle(self, game: games.Game) -> int:
@@ -184,7 +210,9 @@ class Renderer:
     def _render_starting_screen(self):
         wait_for_start_key = True
         while wait_for_start_key:
-            GAME_FONT.render_to(self.screen, (70, 180), "Press X to start..!", (250, 250, 250))
+            GAME_FONT.render_to(
+                self.screen, (70, 180), "Press X to start..!", (250, 250, 250)
+            )
             pygame.display.flip()
 
             for event in pygame.event.get():
@@ -193,7 +221,9 @@ class Renderer:
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_x:
                     wait_for_start_key = False
 
-    def _render(self, game: games.Game, show_sight: Optional[characters.Champion]) -> None:
+    def _render(
+        self, game: games.Game, show_sight: Optional[characters.Champion]
+    ) -> None:
         background = pygame.Surface(self.screen.get_size())
         background.convert()
         self._render_arena(game, background)
@@ -209,7 +239,7 @@ class Renderer:
                     blit_destination[0],
                     blit_destination[1] - HEALTH_BAR_HEIGHT - 1,
                     health * HEALTH_BAR_UNIT_WIDTH,
-                    HEALTH_BAR_HEIGHT
+                    HEALTH_BAR_HEIGHT,
                 )
 
             character_sprite = self.sprite_repository.match_sprite(tile.character)
@@ -217,21 +247,26 @@ class Renderer:
             pygame.draw.rect(
                 background,
                 HEALTH_BAR_OVERFILL_COLOR,
-                prepare_heath_bar_rect(tile.character.health)
+                prepare_heath_bar_rect(tile.character.health),
             )
             pygame.draw.rect(
                 background,
                 HEALTH_BAR_EMPTY_COLOR,
-                prepare_heath_bar_rect(characters.CHAMPION_STARTING_HP)
+                prepare_heath_bar_rect(characters.CHAMPION_STARTING_HP),
             )
             pygame.draw.rect(
                 background,
                 HEALTH_BAR_FULL_COLOR,
-                prepare_heath_bar_rect(min(characters.CHAMPION_STARTING_HP, tile.character.health))
+                prepare_heath_bar_rect(
+                    min(characters.CHAMPION_STARTING_HP, tile.character.health)
+                ),
             )
 
         for i, j in game.arena.terrain:
-            blit_destination = (i * self.sprite_repository.size[0], j * self.sprite_repository.size[1])
+            blit_destination = (
+                i * self.sprite_repository.size[0],
+                j * self.sprite_repository.size[1],
+            )
             tile = game.arena.terrain[i, j]
             tile_sprite = self.sprite_repository.match_sprite(tile)
             background.blit(tile_sprite, blit_destination)
@@ -248,7 +283,12 @@ class Renderer:
                     effect_sprite = self.sprite_repository.match_sprite(effect)
                     background.blit(effect_sprite, blit_destination)
 
-    def _render_sight(self, game: games.Game, show_sight: characters.Champion, background: pygame.Surface) -> None:
+    def _render_sight(
+        self,
+        game: games.Game,
+        show_sight: characters.Champion,
+        background: pygame.Surface,
+    ) -> None:
         if show_sight in game.champions:
             darken_percent = 0.5
             dark = pygame.Surface(self.sprite_repository.size, pygame.SRCALPHA)
@@ -256,5 +296,8 @@ class Renderer:
             visible = game.arena.visible_coords(show_sight)
             for i, j in game.arena.terrain:
                 if (i, j) not in visible:
-                    blit_destination = (i * self.sprite_repository.size[0], j * self.sprite_repository.size[1])
+                    blit_destination = (
+                        i * self.sprite_repository.size[0],
+                        j * self.sprite_repository.size[1],
+                    )
                     background.blit(dark, blit_destination)
